1dd9da0857727f5f36b247d628b05021690fc53c
Vo Thanh Tung <thanhtungka91@gmail.com>
2020-09-16 11:26:21 +0900

    remove bin file

--- map/sum.go
+++ map/sum.go
@@ -1,34 +1,88 @@
 package main
 
 import "fmt"
-// https://play.golang.org/p/LCv9YRFgbVN
-// https://leetcode.com/problems/subarray-sum-equals-k/
-func main() {
-	nums := []int{1}
-	k := 1
-	x := subarraySum(nums, k)
-	fmt.Println("result", x)
+
+type Person struct {
+    StreetAddress, Postcode, City string
+    CompanyName, Position string
+    AnnualIncome int
 }
 
-func subarraySum(nums []int, k int) int {
-    var preSumMap = make(map[int]int)
-    preSumMap[0] = 1
-    var result = 0
-    var sum = 0
-    for _, value := range nums {
-        sum += value
-		
-        tmp := sum-k
-        if _, ok := preSumMap[tmp]; ok {
-            result +=  preSumMap[tmp]
-        }
-        
-        if _, ok := preSumMap[sum]; ok {
-            preSumMap[sum] += 1
-        } else {
-		   
-            preSumMap[sum] = 1
-        }
-    }
-    return result
+type PersonBuilder struct {
+    person *Person // needs to be inited
+}
+
+func NewPersonBuilder() *PersonBuilder {
+    return &PersonBuilder{&Person{}}
+}
+
+func (it *PersonBuilder) Build() *Person {
+    return it.person
+}
+
+func (it *PersonBuilder) Works() *PersonJobBuilder {
+    return &PersonJobBuilder{*it}
+}
+
+func (it *PersonBuilder) Lives() *PersonAddressBuilder {
+    return &PersonAddressBuilder{*it}
+}
+
+type PersonJobBuilder struct {
+    PersonBuilder
+}
+
+func (pjb *PersonJobBuilder) At(
+    companyName string) *PersonJobBuilder {
+    pjb.person.CompanyName = companyName
+    return pjb
+}
+
+func (pjb *PersonJobBuilder) AsA(
+    position string) *PersonJobBuilder {
+    pjb.person.Position = position
+    return pjb
+}
+
+func (pjb *PersonJobBuilder) Earning(
+    annualIncome int) *PersonJobBuilder {
+    pjb.person.AnnualIncome = annualIncome
+    return pjb
+}
+
+type PersonAddressBuilder struct {
+    PersonBuilder
+}
+
+func (it *PersonAddressBuilder) At(
+    streetAddress string) *PersonAddressBuilder {
+    it.person.StreetAddress = streetAddress
+    return it
+}
+
+func (it *PersonAddressBuilder) In(
+    city string) *PersonAddressBuilder {
+    it.person.City = city
+    return it
+}
+
+func (it *PersonAddressBuilder) WithPostcode(
+    postcode string) *PersonAddressBuilder {
+    it.person.Postcode = postcode
+    return it
+}
+
+func main() {
+    pb := NewPersonBuilder()
+    pb.
+        Lives().
+        At("123 London Road").
+        In("London").
+        WithPostcode("SW12BC").
+        Works().
+        At("Fabrikam").
+        AsA("Programmer").
+        Earning(123000)
+    person := pb.Build()
+    fmt.Println(*person)
 }
